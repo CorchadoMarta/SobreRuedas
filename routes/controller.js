83dda6012080098cde284976901def3add1782da
jolgo80 <juan.olgo@gmail.com>
2016-04-16 20:53:03 +0200

    limpieza de código

--- routes/controller.js
+++ routes/controller.js
@@ -5,134 +5,30 @@ var usuarios = require('../model/usuarios.js');
 
 module.exports = function(app, passport){
 
-/*	var registrar = function(req, res) {
-		
-		var contenido = req.body;
-		var usuario = new usuarios(contenido);
-		usuario.direccion = contenido;
-		usuario.markModified('direccion');
-		console.log(contenido);
-		usuario.save();
-			var foto;
- 
-		if (!req.files) {
-			res.send('No se subió tu foto.');
-			return;
-		}
-	 
-		foto = req.files.foto;
-		foto.mv('/public/users', function(err) {
-			if (err) {
-				res.status(500).send(err);
-			}
-			else {
-				res.send('Foto subida!');
-			}
+	app.get('/logout',function(req,res){
+		req.session.destroy(function(err) {
+		    if(err) {
+		    	console.log(err);
+		    } else {
+		    	res.redirect('/');
+		    }
 		});
-	}*/
-	//Funcio per a llistar totes les pelis
-/*	llistaPelis = function(req, res) {
-		//Busquem les pelis
-		pelis.find(function (err, pelicules) {
-			if (!err){
-				//res.send(Pelicules);
-				//Les mostrem enviantles al jade corresponent
-				res.render('mostrarPelis.jade', {movies: pelicules});
-				console.log("Mostrant pelis");
-			} else {
-				//res.send('Error' + err);
-				console.log("No mostra pelis.");
-			}
-		});
-	}
-
-	//Per mostrar la pelicula de la base de dades segons la seva clau
-	var mostraPeli = function(req, res) {
-		//Guardem en una variable el recollit al form
-		var claupeli = Number(req.body.codpeli);
-		//Busquem aquesta pelicula
-		pelis.find({codPeli:claupeli}, function (err, pelicules) {
-			if (!err){
-				//Mostrem la pelicula enviantlo al jade corresponent
-				res.render('mostrarUnaPeli.jade', {movie: pelicules});
-				//res.send(Pelicules);
-				console.log("Mostrant peli");
-			} else {
-				//res.send('Error' + err);
-				console.log("No mostra pelis.");
-			}
-		});
-	}
-
-	//Eliminar una pelicula de la base de dades
-	var deletePeli = function(req, res) {
-		//Guardem en una variable el recollit al form
-		var idpeli = Number(req.body.codpeli);
-		res.header("utf8");
-		//Eliminem aquesta pelicula
-		pelis.remove({codPeli:idpeli}, function (err){
-			if(!err) {
-				//res.send("Pelicula eliminada");
-				res.render('missatgeEliminaCorrecte.jade', {movie: idpeli});
-			} else {
-				res.render('error.jade');
-			}
-		});
-	}
-
-
-
-	//Per cambiar la pelicula a la base de dades segons la seva clau
-	var updatePeli = function(req, res) {
-		//Recollim les dades del form
-		var claupeli = Number(req.body.codpeli);
-		var titolpeli = req.body.titol;
-		var sinopsipeli = req.body.sinopsi;
-		//Mirem si la pelicula
-		pelis.find({codPeli:claupeli}, function (err, pelicula) {
-			if (!err && pelicula.length != 0){
-				pelis.update({codPeli:claupeli}, {$set: {titol: titolpeli, sinopsi: sinopsipeli}}, function (err, pelicula) {
-					if (!err){
-						res.render('missatgeUpdateInsert.jade', {accio: "Update"});
-						//res.send(Pelicules);
-						console.log("Mostrant pelis");
-					} else {
-						//res.send('Error' + err);
-						console.log("No mostra pelis.");
-					}
-				});
-				console.log("No la pots inserir");
-			} else {
-				//res.send('Error' + err);
-				console.log("No update pelis.");
-			}
-		});
-	}
-
-*/
-	 app.get('/logout',function(req,res){
-	 req.session.destroy(function(err) {
-	   if(err) {
-	     console.log(err);
-	   } else {
-	     res.redirect('/');
-	   }
-	 });
 	});
 
 	app.get('/', function(req, res) {
 		res.render('index.ejs',
-		 {botonRegistro: 'partials/registro'});
+		{botonRegistro: 'partials/registro'});
 	});
 
 	app.get('/registro', function(req, res) {
 		res.render('registro.ejs',  { botonRegistro: 'partials/registro'});
 	});
+	
 	app.get('/bienvenido', isLoggedIn , function(req, res) {
 		res.render('bienvenido.ejs', { botonRegistro: 'partials/piolin'});
 
 	});
-/*	app.post('/registrar', registrar);*/
+
 	// process the login form
     app.post('/registrar', passport.authenticate('local-signup', {
         successRedirect : '/bienvenido', // redirect to the secure bienvenido section
@@ -145,31 +41,6 @@ module.exports = function(app, passport){
         failureRedirect : '/', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
-
-/*	app.get('/list', llistaPelis);
-	//Per mostrar una
-	app.get('/listOne', function(req, res) {
-		res.render('formMostrarPeli.jade');
-	});
-	app.post('/mostraPeli', mostraPeli);
-	//Per eliminar una
-	app.get('/deleteOne', function(req, res) {
-		res.render('formEliminarPeli.jade');
-	});
-	app.post('/deletePeli', deletePeli);
-	//Per inserir una
-	//Per cambiar una
-	app.get('/updateOne', function(req, res) {
-		res.render('formUpdatePeli.jade');
-	});
-	app.post('/updatePeli', updatePeli);
-	    // process the signup form*/
-    app.post('/registrar', passport.authenticate('local-signup', {
-        successRedirect : '/bienvenido', // redirect to the secure bienvenido section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
 };
 
 // route middleware to make sure a user is logged in
