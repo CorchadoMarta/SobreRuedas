1fbb98c0bd9040f32f5d71b0c203770217c7ba6d
Marta <gcatram@gmail.com>
2016-05-10 20:41:53 +0200

    Edit profile

--- public/js/datosUser.js
+++ public/js/datosUser.js
@@ -6,14 +6,31 @@ angular.module('datosUser').controller('datosUsuario', ['$scope', '$http' , func
     $scope.datosUserFunction = function () {
 
         $http.get('/datosUser')
-        .success(function(datoDeUsuario) {
+            .success(function(datoDeUsuario) {
             console.log(datoDeUsuario);
             $scope.datos =  datoDeUsuario;
         })
-        .error(function(datoDeUsuario) {
+            .error(function(datoDeUsuario) {
             console.log('Error: ' + datoDeUsuario);
         });
     };
+
+    $scope.editor = false;
+
+    $scope.editar = function() {
+        $scope.editor = true;
+        $scope.editaNombre = $scope.datos[0].nombre;
+    };
+
+    $scope.cancelar = function() {
+        $scope.editor = false;
+    };
+
+    $scope.guardar = function() {
+        $scope.datos[0].nombre = $scope.editaNombre;
+        $scope.cancelar();
+    };
+
 }]);
 
 
